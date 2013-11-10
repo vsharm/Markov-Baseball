5291f8114608cc3f5764db38f51aee5fef262935
vsharm <varun.sharma@reflektion.com>
2013-11-09 21:11:39 -0800

    Update markov_baseball.py
    Updated with old run matrix and fixed the out error. Added new run vector.

--- markov_baseball.py
+++ markov_baseball.py
@@ -1,17 +1,17 @@
 import numpy
 from numpy import linalg
-singles = 1477
-doubles = 560
-triples = 47
-hr = 377
-plateAppearences = 9299
-walks = 801
+singles = 28438
+doubles = 8222
+triples = 772
+hr = 4661
+plateAppearences = 184872
+walks = 14640
 
 pSingle = singles/float(plateAppearences)
 pDouble = doubles/float(plateAppearences)
 pTriple = triples/float(plateAppearences)
 pHR = hr/float(plateAppearences)
-pOut = 1 - ((singles+doubles+triples+hr)/float(plateAppearences))
+pOut = 1 - ((singles+doubles+triples+hr + walks)/float(plateAppearences))
 pWalk = walks/float(plateAppearences)
 pWalkSingle = (walks+singles)/float(plateAppearences)
 
@@ -24,35 +24,35 @@ pSingleRii = (3*pSingle)/4
 
 
 
-print "Stats", pSingle, pDouble, pTriple, pHR, pWalkSingle, pOut, pWalk
+print("Stats", pSingle, pDouble, pTriple, pHR, pWalkSingle, pOut, pWalk, (pSingle + pDouble + pTriple + pHR + pWalkSingle + pOut + pWalk))
 
 
 transition = numpy.matrix([
                            [pHR,pWalkSingle,pDouble,pTriple,0,0,0,0,pOut,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-                           [pHR,0,pDoubleR,pTriple,pWalkSingleR,pSingleR,pDoubleR,0,0,pOut,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-                           [pHR,pSingleRii,pDouble,pTriple,pWalk,pSingleRi,0,0,0,0,pOut,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                           [pHR,0,0,pTriple,pWalkSingle,0,pDouble,0,0,pOut,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+                           [pHR,pSingle,pDouble,pTriple,pWalk,0,0,0,0,0,pOut,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
                            [pHR,pSingle,pDouble,pTriple,0,pWalk,0,0,0,0,0,pOut,0,0,0,0,0,0,0,0,0,0,0,0,0],
-                           [pHR,0,pDoubleR,pTriple,pSingleRi,pSingleR,pDoubleR,pWalkSingleR,0,0,0,0,pOut,0,0,0,0,0,0,0,0,0,0,0,0],
-                           [pHR,0,pDoubleR,pTriple,pSingleR,pSingleR,pDoubleR,pWalk,0,0,0,0,0,pOut,0,0,0,0,0,0,0,0,0,0,0],
-                           [pHR,pSingleRi,pDouble,pTriple,0,pSingleR,0,pWalk,0,0,0,0,0,0,pOut,0,0,0,0,0,0,0,0,0,0],
-                           [pHR,0,pDoubleR,pTriple,pSingleR,pSingleR,pDoubleR,pWalk,0,0,0,0,0,0,0,pOut,0,0,0,0,0,0,0,0,0],
+                           [pHR,0,0,pTriple,pSingle,0,pDouble,pWalk,0,0,0,0,pOut,0,0,0,0,0,0,0,0,0,0,0,0],
+                           [pHR,0,0,pTriple,pSingle,0,pDouble,pWalk,0,0,0,0,0,pOut,0,0,0,0,0,0,0,0,0,0,0],
+                           [pHR,pSingle,pDouble,pTriple,0,0,0,pWalk,0,0,0,0,0,0,pOut,0,0,0,0,0,0,0,0,0,0],
+                           [pHR,0,0,pTriple,pSingle,0,pDouble,pWalk,0,0,0,0,0,0,0,pOut,0,0,0,0,0,0,0,0,0],
                            [0,0,0,0,0,0,0,0,pHR,pWalkSingle,pDouble,pTriple,0,0,0,0,pOut,0,0,0,0,0,0,0,0],
-                           [0,0,0,0,0,0,0,0,pHR,0,pDoubleR,pTriple,pWalkSingleR,pSingleR,pDoubleR,0,0,pOut,0,0,0,0,0,0,0],
-                           [0,0,0,0,0,0,0,0,pHR,pSingleRii,pDouble,pTriple,pWalk,pSingleRi,0,0,0,0,pOut,0,0,0,0,0,0],
+                           [0,0,0,0,0,0,0,0,pHR,0,0,pTriple,pWalkSingle,0,pDouble,0,0,pOut,0,0,0,0,0,0,0],
+                           [0,0,0,0,0,0,0,0,pHR,pSingle,pDouble,pTriple,pWalk,0,0,0,0,0,pOut,0,0,0,0,0,0],
                            [0,0,0,0,0,0,0,0,pHR,pSingle,pDouble,pTriple,0,pWalk,0,0,0,0,0,pOut,0,0,0,0,0],
-                           [0,0,0,0,0,0,0,0,pHR,0,pDoubleR,pTriple,pSingleRi,pSingleR,pDoubleR,pWalkSingleR,0,0,0,0,pOut,0,0,0,0],
-                           [0,0,0,0,0,0,0,0,pHR,0,pDoubleR,pTriple,pSingleR,pSingleR,pDoubleR,pWalk,0,0,0,0,0,pOut,0,0,0],
-                           [0,0,0,0,0,0,0,0,pHR,pSingleRi,pDouble,pTriple,0,pSingleR,0,pWalk,0,0,0,0,0,0,pOut,0,0],
-                           [0,0,0,0,0,0,0,0,pHR,0,pDoubleR,pTriple,pSingleR,pSingleR,pDoubleR,pWalk,0,0,0,0,0,0,0,pOut,0],
+                           [0,0,0,0,0,0,0,0,pHR,0,0,pTriple,pSingle,0,pDouble,pWalk,0,0,0,0,pOut,0,0,0,0],
+                           [0,0,0,0,0,0,0,0,pHR,0,0,pTriple,pSingle,0,pDouble,pWalk,0,0,0,0,0,pOut,0,0,0],
+                           [0,0,0,0,0,0,0,0,pHR,pSingle,pDouble,pTriple,0,0,0,pWalk,0,0,0,0,0,0,pOut,0,0],
+                           [0,0,0,0,0,0,0,0,pHR,0,0,pTriple,pSingle,0,pDouble,pWalk,0,0,0,0,0,0,0,pOut,0],
                            [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,pHR,pWalkSingle,pDouble,pTriple,0,0,0,0,pOut],
-                           [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,pHR,0,pDoubleR,pTriple,pWalkSingleR,pSingleR,pDoubleR,0,pOut],
-                           [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,pHR,pSingleRii,pDouble,pTriple,pWalk,pSingleRi,0,0,pOut],
+                           [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,pHR,0,0,pTriple,pWalkSingle,0,pDouble,0,pOut],
+                           [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,pHR,pSingle,pDouble,pTriple,pWalk,0,0,0,pOut],
                            [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,pHR,pSingle,pDouble,pTriple,0,pWalk,0,0,pOut],
-                           [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,pHR,0,pDoubleR,pTriple,pSingleRi,pSingleR,pDoubleR,pWalkSingleR,pOut],
-                           [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,pHR,0,pDoubleR,pTriple,pSingleR,pSingleR,pDoubleR,pWalk,pOut],
-                           [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,pHR,pSingleRi,pDouble,pTriple,0,pSingleR,0,pWalk,pOut],
-                           [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,pHR,0,pDoubleR,pTriple,pSingleR,pSingleR,pDoubleR,pWalk,pOut],
-                           [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+                           [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,pHR,0,0,pTriple,pSingle,0,pDouble,pWalk,pOut],
+                           [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,pHR,0,0,pTriple,pSingle,0,pDouble,pWalk,pOut],
+                           [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,pHR,pSingle,pDouble,pTriple,0,0,0,pWalk,pOut],
+                           [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,pHR,0,0,pTriple,pSingle,0,pDouble,pWalk,pOut],
+                           [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1]
                            ])
 
 runVector  = numpy.matrix([
@@ -89,5 +89,5 @@ for z in  range(1,8):
     tempMatrix = numpy.dot(curentTransition,runVector)
     resultMatrix = numpy.add(resultMatrix,tempMatrix)
 
-print "resultMatrix"
-print resultMatrix
+print("resultMatrix")
+print(resultMatrix)
